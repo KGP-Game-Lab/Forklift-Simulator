9b3c360e00e33ea13cc9de040ee4cb8f37bc332b
amrit-choudhary <albela.amrit@gmail.com>
2018-08-31 00:38:19 +0530

    camera

---
+++ ForkliftSimulator/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+
+    public List<Camera> cameras;
+    private int currentCamerIndex = 0;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            currentCamerIndex++;
+            
+			if (currentCamerIndex > cameras.Count - 1)
+                currentCamerIndex = 0;
+
+			cameras.ForEach(X => X.gameObject.SetActive(false));
+			cameras[currentCamerIndex].gameObject.SetActive(true);
+        }
+    }
+}
